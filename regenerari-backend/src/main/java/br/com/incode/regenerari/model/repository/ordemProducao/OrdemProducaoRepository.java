a00e4a869fe94a1092495a29b98d17a5fb4060a0
leandrolimasi <leandrolimadasilva@gmail.com>
2017-06-11 09:40:08 -0300

    fix: production order saving status 'finalizada'

--- regenerari-backend/src/main/java/br/com/incode/regenerari/model/repository/ordemProducao/OrdemProducaoRepository.java
+++ regenerari-backend/src/main/java/br/com/incode/regenerari/model/repository/ordemProducao/OrdemProducaoRepository.java
@@ -1,7 +1,14 @@
 package br.com.incode.regenerari.model.repository.ordemProducao;
 
 import br.com.incode.regenerari.entity.OrdemProducaoEntity;
+import com.powerlogic.jcompany.core.commons.search.PlcPagedResult;
+import com.powerlogic.jcompany.core.commons.search.PlcPagination;
+import com.powerlogic.jcompany.core.model.qbe.OrderBy;
+import com.powerlogic.jcompany.core.model.qbe.OrderByDirection;
+import com.powerlogic.jcompany.core.model.qbe.SearchParameters;
 import com.powerlogic.jcompany.core.model.repository.PlcAbstractRepository;
+
+import javax.persistence.metamodel.ManagedType;
 
 /**
  * Created by leandrolimadasilva on 26/05/17.
@@ -15,4 +22,16 @@ public class OrdemProducaoRepository extends PlcAbstractRepository<Long, OrdemPr
     }
 
 
+    @Override
+    public PlcPagedResult<OrdemProducaoEntity> findPaged(OrdemProducaoEntity entity, SearchParameters sp,
+                                                         PlcPagination<OrdemProducaoEntity> config) {
+
+        ManagedType<OrdemProducaoEntity> mt = getEntityManager().getMetamodel().entity(OrdemProducaoEntity.class);
+        sp.addOrderBy(new OrderBy(OrderByDirection.DESC, mt.getAttribute("statusOrdemProducao")));
+        sp.addOrderBy(new OrderBy(OrderByDirection.ASC, mt.getAttribute("motivoOrdemProducao")));
+        sp.addOrderBy(new OrderBy(OrderByDirection.ASC, mt.getAttribute("numero")));
+        sp.caseInsensitive().anywhere();
+
+        return super.findPaged(entity, sp, config);
+    }
 }
--- regenerari-frontend/src/app/estoqueProduto/estoque.produto.controller.js
+++ regenerari-frontend/src/app/estoqueProduto/estoque.produto.controller.js
@@ -16,8 +16,10 @@
       { field: 'produto.codigo', displayName: 'Cód.'},
       { field: 'produto.nome', displayName: 'Nome'},
       { field: 'quantidade', displayName: 'Quantidade', cellFilter: 'finance:false:3'},
-      { field: 'valorUnitario', displayName: 'Valor Un.', cellFilter: 'finance:true:3'}
+      { field: 'eventoEstoque', displayName: 'Evento'},
+      { field: 'dataUltimaAlteracao', displayName: 'Data', cellFilter: 'date'}
     ];
+
 
 
     vm.afterInitialize = function(){
